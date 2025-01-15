8a9106c26ee2f103c86753b65faf0ec99de3d9ab
furkan yuksel <99129857+fyt19@users.noreply.github.com>
2025-01-15 04:24:53 +0300

    Update degiskenler.php

--- degiskenler.php
+++ degiskenler.php
@@ -5,3 +5,26 @@ $ortalama = 85.6;
 
 echo "İsim: $isim, Yaş: $yas, Ortalama: $ortalama";
 ?>
+
+<?php
+$globalDegisken = "Bu global bir değişkendir.";
+
+function yazdir() {
+    global $globalDegisken;
+    echo $globalDegisken;
+}
+
+yazdir(); // Çıktı: Bu global bir değişkendir.
+?>
+<?php
+function sayac() {
+    static $sayi = 0;
+    $sayi++;
+    echo $sayi . "<br>";
+}
+
+sayac(); // 1
+sayac(); // 2
+sayac(); // 3
+?>
+
