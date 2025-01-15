e6da09bc3078b9653fff1b38d0d9b67eae79b6df
furkan yuksel <99129857+fyt19@users.noreply.github.com>
2025-01-15 04:26:45 +0300

    Create fonksiyonlar.php

---
+++ fonksiyonlar.php
@@ -0,0 +1,19 @@
+<?php
+// Değer Alan ve Döndüren:
+function toplama($a, $b) {
+    return $a + $b;
+}
+
+echo toplama(5, 10); // Çıktı: 15
+?>
+
+
+
+<?php
+// Değer Almayan ve Ekrana Yazdıran:
+function selamla() {
+    echo "Merhaba!";
+}
+
+selamla(); // Çıktı: Merhaba!
+?>
