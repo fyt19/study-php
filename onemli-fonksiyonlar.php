9f736d7e1a2e3697ac089e85387abda563948f46
furkan yuksel <99129857+fyt19@users.noreply.github.com>
2025-01-15 04:27:58 +0300

    Create onemli-fonksiyonlar.php

---
+++ onemli-fonksiyonlar.php
@@ -0,0 +1,18 @@
+<?php
+$sayi = 123;
+echo gettype($sayi); // integer
+?>
+
+<?php
+echo rand(1, 100); // 1 ile 100 arasında rastgele bir sayı
+?>
+
+<?php
+$dizi = [1, "merhaba", 3.14];
+var_dump($dizi);
+?>
+
+<?php
+$metin = "PHP öğreniyorum.";
+echo strlen($metin); // 17
+?>
